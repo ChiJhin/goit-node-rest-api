69154fd38e9f30d0db03d346b171eeb6b7c4a8a5
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-04-22 21:45:54 +0300

    hw 04 bug fix

--- services/contactServises.js
+++ services/contactServises.js
@@ -7,18 +7,18 @@ export const getContacts = (id) => Contacts.find({ owner: id });
 
 export const deleteChooseContact = (id, owner) => {
     const contact = Contacts.findById(id)
-    if (!contact.id === owner) throw new HttpError(409, "Not your contact!")
+    if (contact.owner !== owner) throw new HttpError(409, "Not your contact!")
     Contacts.findOneAndDelete(id)
 }
 
 export const updateChooseContact = (id, params, owner) => {
     const contact = Contacts.findById(id)
-    if (!contact.id === owner) throw new HttpError(409, "Not your contact!")
+    if (contact.owner !== owner) throw new HttpError(409, "Not your contact!")
     Contacts.findOneAndUpdate(id, params, {new: true});
 }
 
 export const changeStatus = (id, value, owner) => {
     const contact = Contacts.findById(id)
-    if (!contact.id === owner) throw new HttpError(409, "Not your contact!")
+    if (contact.owner !== owner) throw new HttpError(409, "Not your contact!")
     Contacts.findOneAndUpdate(id, value, {new: true});
 }
