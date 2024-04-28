ee4fb0467e6934ef41351117f8e64245449f77c0
Dimitry Minchukov <jerek.d.lem@gmail.com>
2024-04-28 22:56:20 +0300

    hw 05 bug fix

--- services/avatarServise.js
+++ services/avatarServise.js
@@ -1,24 +1,21 @@
-import multer from "multer";
-import { nanoid } from "nanoid";
-import { promises as fs } from "fs";
-import path from "path";
-import jimp from "jimp";
-import fse from "fs-extra";
+import multer from 'multer';
+import { nanoid } from 'nanoid';
+import { promises as fs } from 'fs';
+import path from 'path';
+import jimp from 'jimp';
+import fse from 'fs-extra';
 
-import HttpError from "../helpers/HttpError.js";
+import HttpError from '../helpers/HttpError.js';
 
 export class ImageService {
   static initUploadImageMiddleware(fieldName) {
     const multerStorage = multer.memoryStorage();
 
     const multerFilter = (req, file, cb) => {
-      if (file.mimetype.startsWith("image/")) {
+      if (file.mimetype.startsWith('image/')) {
         cb(null, true);
       } else {
-        cb(
-          new HttpError(400, "Not an image! Please upload only images."),
-          false
-        );
+        cb(new HttpError(400, 'Not an image! Please upload only images.'), false);
       }
     };
     return multer({
@@ -28,26 +25,21 @@ export class ImageService {
   }
 
   static async saveImage(file, options, ...pathSegments) {
-    if (
-      file.size >
-      (options?.maxFileSize
-        ? options.maxFileSize * 1024 * 1024
-        : 1 * 1024 * 1024)
-    ) {
-      throw new HttpError(400, "File is too large..");
+    if (file.size > (options?.maxFileSize ? options.maxFileSize * 1024 * 1024 : 1 * 1024 * 1024)) {
+      throw new HttpError(400, 'File is too large..');
     }
-    const fileorig = path.join(process.cwd(), "tmp", file.originalname);
+    const fileorig = path.join(process.cwd(), 'tmp', file.originalname);
 
     fse.ensureFileSync(fileorig); // create file if not exists
-    await fs.writeFile(fileorig, file.buffer);  
+    await fs.writeFile(fileorig, file.buffer);
 
-    const fullFilePath = path.join(process.cwd(), "public", ...pathSegments);
-    const fileName = `${nanoid()}.jpeg`; 
+    const fullFilePath = path.join(process.cwd(), 'public', ...pathSegments);
+    const fileName = `${nanoid()}.jpeg`;
     const avatar = await jimp.read(file.buffer);
     await avatar
-    .cover(options?.width ?? 250, options?.height ?? 250)
-    .quality(90)
-    .writeAsync(path.join(fullFilePath, fileName));
+      .cover(options?.width ?? 250, options?.height ?? 250)
+      .quality(90)
+      .writeAsync(path.join(fullFilePath, fileName));
 
     await fse.remove(fileorig);
     return path.join(...pathSegments, fileName);
