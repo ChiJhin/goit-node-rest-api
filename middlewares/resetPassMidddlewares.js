872efeb4b82dbccd4677e85e1eb1e8cf217dc181
Dimitry Minchukov <112732061+ChiJhin@users.noreply.github.com>
2024-05-06 14:54:36 +0300

    bug fix

--- middlewares/resetPassMidddlewares.js
+++ middlewares/resetPassMidddlewares.js
@@ -1,10 +1,9 @@
-import { catchAsync } from '../helpers/catchAsync.js';
 import { emailSchema } from '../schemas/resetSchemas.js';
 import HttpError from '../helpers/HttpError.js';
 
-export const checkEmailVerification = catchAsync((req, res, next) => {
+export const checkEmailVerification = (req, res, next) => {
   const { value, errors } = emailSchema(req.body);
   if (errors) throw new HttpError(400, errors);
   req.body = value;
   next();
-});
+};
